747e67e1eda1cf926f1b9ddada902874e6f12b9c
chynnalew <chynnalew@yahoo.com>
2021-09-10 11:37:56 -0700

    add functionality to alert error codes

--- src/currency.js
+++ src/currency.js
@@ -3,29 +3,14 @@ export class ExchangeRate {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
       .then(function(response) {
         if(!response.ok) {
-          throw Error(response.statusText);
+          throw Error(response.status);
         }
         return response.json();
       })
       .catch(function(error) {
-        return error;
-      })
+        alert(error);
+      });
   }
-  // static getRates(currency) {
-  //   return new Promise(function(resolve, reject) {
-  //     let request = new XMLHttpRequest();
-  //     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
-  //     request.onload = function() {
-  //       if (request.readyState === 4 || this.status === 200) {
-  //         resolve(request.response);
-  //       } else {
-  //         reject(request.response);
-  //       }
-  //     };
-  //     request.open("GET", url, true);
-  //     request.send();
-  //   });
-  // }
 }
 
 export function convert(usd, rate){

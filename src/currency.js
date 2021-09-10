b7baa793a05761a05dd5b922901ea255809bc5b8
chynnalew <chynnalew@yahoo.com>
2021-09-10 11:02:48 -0700

    refactor currency.js, index.js to use fetch

--- src/currency.js
+++ src/currency.js
@@ -1,19 +1,31 @@
 export class ExchangeRate {  
   static getRates(currency) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
-      request.onload = function() {
-        if (request.readyState === 4 || this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+      .then(function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
         }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
   }
+  // static getRates(currency) {
+  //   return new Promise(function(resolve, reject) {
+  //     let request = new XMLHttpRequest();
+  //     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
+  //     request.onload = function() {
+  //       if (request.readyState === 4 || this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(request.response);
+  //       }
+  //     };
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
 }
 
 export function convert(usd, rate){
--- src/index.js
+++ src/index.js
@@ -8,25 +8,25 @@ import {ExchangeRate, convert} from './currency.js';
 $(document).ready(function() {
   $('#convert1').click(function() {
     let currency = $('#currency').val();
-    let promise = ExchangeRate.getRates(currency);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      let rate = body.conversion_rates.USD;
-      let usd = $('#userNumber1').val();
-      let newVal = convert(usd, rate);
-      $("#conversion1").html(newVal);
-    })
+    ExchangeRate.getRates(currency)
+      .then(function(response) {
+        const body = response;
+        let rate = body.conversion_rates.USD;
+        let usd = $('#userNumber1').val();
+        let newVal = convert(usd, rate);
+        $("#conversion1").html(newVal);
+      });
   });
   $('#convert2').click(function() {
     let currency = $('#currency2').val();
-    let promise = ExchangeRate.getRates(currency);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      let rate = body.conversion_rates.USD;
-      let usd = $('#userNumber2').val();
-      let newVal = convert(usd, rate);
-      $("#conversion2").html(newVal);
-    })
+    ExchangeRate.getRates(currency)
+      .then(function(response) {
+        const body = response;
+        let rate = body.conversion_rates.USD;
+        let usd = $('#userNumber2').val();
+        let newVal = convert(usd, rate);
+        $("#conversion2").html(newVal);
+      });
   });
 });
 
